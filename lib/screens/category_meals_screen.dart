927e09a1d00de2e4a9ed5b74bc36194822e346c5
Korkmatik <9c54efc1-143b-4f5f-9d2f-f6f189583f0f@aleeas.com>
2021-10-09 20:20:14 +0200

    Refactored screens and widgets into directories - Iterate over meals

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../dummy_data.dart';
+
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
 //  final String categoryId;
@@ -12,14 +14,15 @@ class CategoryMealsScreen extends StatelessWidget {
     final routeArgs = ModalRoute.of(context)!.settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'] as String;
     final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) => meal.categories.contains(categoryId)).toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: const Center(
-        child: Text('The Recipes for the category!'),
-      ),
+      body: ListView.builder(itemBuilder: (ctx, i) {
+        return Text(categoryMeals[i].title);
+      }, itemCount: categoryMeals.length,),
     );
   }
 }
